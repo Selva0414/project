0cb07f3ebb5111288fdad2c16f3d62d706c7b4e7
selva <selv39629@gmail.com>
2026-03-03 01:44:18 +0530

    Add health check root route

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,6 +26,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Root route for health check
+app.get('/', (req, res) => {
+    res.send('Product Management API is running...');
+});
+
 // API Endpoints
 
 // Create Product
